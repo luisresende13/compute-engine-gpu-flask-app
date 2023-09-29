60043d6c6b614760ad95f47a1c064f008a6bd17d
Luis Resende Silva <luisresende13@gmail.com>
2023-09-29 18:27:30 -0300

    Update startup-script.sh paths

--- startup-script.sh
+++ startup-script.sh
@@ -26,21 +26,21 @@ export HOME=/root
 git clone https://github.com/GoogleCloudPlatform/getting-started-python.git /opt/app
 
 # Install Cloud Ops Agent
-sudo bash /opt/app/gce/add-google-cloud-ops-agent-repo.sh --also-install
+sudo bash /opt/app/add-google-cloud-ops-agent-repo.sh --also-install
 
 # Account to own server process
 useradd -m -d /home/pythonapp pythonapp
 
 # Python environment setup
-virtualenv -p python3 /opt/app/gce/env
-/bin/bash -c "source /opt/app/gce/env/bin/activate"
-/opt/app/gce/env/bin/pip install -r /opt/app/gce/requirements.txt
+virtualenv -p python3 /opt/app/env
+/bin/bash -c "source /opt/app/env/bin/activate"
+/opt/app/env/bin/pip install -r /opt/app/requirements.txt
 
 # Set ownership to newly created account
 chown -R pythonapp:pythonapp /opt/app
 
 # Put supervisor configuration in proper place
-cp /opt/app/gce/python-app.conf /etc/supervisor/conf.d/python-app.conf
+cp /opt/app/python-app.conf /etc/supervisor/conf.d/python-app.conf
 
 # Start service via supervisorctl
 supervisorctl reread
