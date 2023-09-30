02542c56c7f58e9f4674abd7b554eb197bc4ff56
Luis Resende Silva <luisresende13@gmail.com>
2023-09-30 01:12:37 -0300

    pytorch and opencv gpu support test endpoints added

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-# Copyright 2019 Google LLC All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from flask import Flask, jsonify
+import cv2
 import tensorflow as tf
+import torch
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def root():
     return "Server is running!"
 
-@app.route('/gpu', methods=['GET'])
-def gpu():
-    if tf.config.experimental.list_physical_devices('GPU'):
-        return jsonify({'message': 'GPU is available', 'gpu': True})
-    else:
-        return jsonify({'message': 'GPU is not available', 'gpu': False})
+@app.route('/tensorflow-gpu', methods=['GET'])
+def tensorflow_gpu():
+    try:
+        if tf.config.experimental.list_physical_devices('GPU'):
+            return jsonify({'message': 'TensorFlow GPU is available', 'tensorflow_gpu': True})
+        else:
+            return jsonify({'message': 'TensorFlow GPU is not available', 'tensorflow_gpu': False})
+    except Exception as e:
+        return jsonify({'message': 'Error checking TensorFlow GPU availability', 'error': str(e)})
+
+@app.route('/pytorch-gpu', methods=['GET'])
+def pytorch_gpu():
+    try:
+        if torch.cuda.is_available():
+            return jsonify({'message': 'PyTorch GPU is available', 'pytorch_gpu': True})
+        else:
+            return jsonify({'message': 'PyTorch GPU is not available', 'pytorch_gpu': False})
+    except Exception as e:
+        return jsonify({'message': 'Error checking PyTorch GPU availability', 'error': str(e)})
+
+@app.route('/opencv-gpu', methods=['GET'])
+def opencv_gpu():
+    try:
+        if cv2.cuda.getCudaEnabledDeviceCount() > 0:
+            return jsonify({'message': 'OpenCV GPU support is available', 'opencv_gpu': True})
+        else:
+            return jsonify({'message': 'OpenCV GPU support is not available', 'opencv_gpu': False})
+    except Exception as e:
+        return jsonify({'message': 'Error checking OpenCV GPU support', 'error': str(e)})
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
